75371f2697b974daa9478927301a63dee7431fc2
wndgur76@naver.com <wndgur76@naver.com>
2021-02-23 23:31:07 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '이중혁',
+  'birthday': '991108',
+  'gender': '남자',
+  'job': 'soldier'
+},
+{
+'id': 2,
+'image': 'https://placeimg.com/64/64/2',
+'name': '이중혁',
+'birthday': '991103',
+'gender': '남자',
+'job': 'programmer'
+},
+{
+'id': 3,
+'image': 'https://placeimg.com/64/64/3',
+'name': '이중혁',
+'birthday': '991101',
+'gender': '남자',
+'job': 'soldier'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c=>{ return (<Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>);})}
+      </div>
+    );
+  }
 }
 
 export default App;
